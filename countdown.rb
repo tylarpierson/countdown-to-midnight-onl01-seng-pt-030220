be4a91c5f94236f213be33b9dd61989beb813691
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-04 16:13:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(number=12) 
   while number > 0 do 
-    puts "#{number} Second(S)!"
+    puts "#{number} SECOND(S)!"
     number -= 1 
   end
   return "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(number=12) 
   while number > 0 do 
-    puts "#{number} Second(S)!"
+    puts "#{number} SECOND(S)!"
     sleep 5 
     number -= 1 
   end
